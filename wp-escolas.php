5a41a3c28d3cd1d89a570ff8498b8df6ebdc3f87
Joel Garcia Jr <joel.garciajr84@gmail.com>
2014-07-11 00:39:36 -0300

    Comia Inicial

---
+++ wp-escolas.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+* Plugin Name: WP Escolas
+* Plugin URL: http://www.wpescolas.com.br
+* Description: Plugin para Gerenciamento de Entidades de Ensino
+* Author: Joel Garcia Jr
+* Version: 1.0
+* Text Domain: wpescolas
+* Author URL: https://github.com/joelgarciajr84
+*/
